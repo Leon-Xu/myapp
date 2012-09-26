b7f3960dc3d77d55fa5f01f377be3cf282328c2c
Leon Xu <leon.x.xu@gmail.com>
2012-09-26 13:30:39 -0700

    Make a basic User model (including secure passwords)

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,14 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
 
   validates :content, :length => { :maximum => 140 }
-end
+end# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
